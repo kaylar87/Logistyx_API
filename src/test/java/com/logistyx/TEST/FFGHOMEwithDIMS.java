0daa9fc6ec2461610f8cef40f00e3e89f0ece138
Serge Yasinovsky <serge.yasinovsky@logistyx.com>
2022-01-13 18:31:42 -0500

    ref 6.0 - testCases added

--- src/test/java/com/logistyx/TEST/FFGHOMEwithDIMS.java
+++ src/test/java/com/logistyx/TEST/FFGHOMEwithDIMS.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.Calendar;
+import java.util.Locale;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -48,6 +51,7 @@ public class FFGHOMEwithDIMS extends FedExFuseBaseNotDG {
     }
 
 
+    //TODO SEPARATE
     @DisplayName("ZPL: Delivery Address")
     @Test
     public void test4() {
@@ -123,4 +127,199 @@ public class FFGHOMEwithDIMS extends FedExFuseBaseNotDG {
 
     }
 
+
+    @DisplayName("HUF and PDF are generated")
+    @Test
+    public void test7() {
+
+        for (int i = 0; i < fedExFusePojoConveyances.getDocuments().size(); i++) {
+            if (fedExFusePojoConveyances.getDocuments().get(i).getDocumentFormatCode().contains("HUF")) {
+                //    System.out.println("fedExFusePojoConveyances.getDocuments().get(i).getDocumentFormatCode() = " + fedExFusePojoConveyances.getDocuments().get(i).getDocumentFormatCode());
+                assertThat(decodedStringConveyancesEDI, containsString("<TrackingIdType>FEDEX</TrackingIdType>"));
+                System.out.println("HUF is generated");
+            } else if (fedExFusePojoConveyances.getDocuments().get(i).getDocumentFormatCode().contains("PDF")) {
+                System.out.println("Label is generated");
+                assertThat(fedExFusePojoConveyances.getDocuments().get(i).getContent(), is(notNullValue()));
+                assertThat(fedExFusePojoConveyances.getDocuments().get(i).getContent().length(), is(greaterThan(500)));
+            }
+        }
+    }
+
+
+    @DisplayName("ZPL: Pickup Address 'ORIGIN ID:' exists")
+    @Test
+    public void test8() {
+
+        assertThat(decodedStringShipments, containsString("^FT10,27^A0N,22,22^FDORIGIN ID:                     0208080007^FS"));
+
+    }
+
+
+    @DisplayName("ZPL: Pickup Address Contact exists: Contact Name")
+    @Test
+    public void test9() {
+
+        String pickupAddressContactFromJson = fedExFusePojoShipments.getPickupAddress().getContacts().get(0).getName();
+        //    System.out.println("pickupAddressContactFromJson = " + pickupAddressContactFromJson);
+        int pickupAddressContactFromLabelStart = decodedStringShipments.indexOf("^FT10,71^A0N,22,22^FD") + 21;
+        String pickupAddressContactFromLabel = decodedStringShipments.substring(pickupAddressContactFromLabelStart, pickupAddressContactFromLabelStart + pickupAddressContactFromJson.length());
+        //    System.out.println("pickupAddressContactFromLabel = " + pickupAddressContactFromLabel);
+        assertThat(pickupAddressContactFromJson.toUpperCase(), is(equalTo(pickupAddressContactFromLabel)));
+
+    }
+
+
+    @DisplayName("ZPL: Pickup Address Company exists: Company Name")
+    @Test
+    public void test10() {
+
+        String pickupAddressCompanyFromJson = fedExFusePojoShipments.getPickupAddress().getReference();
+        //    System.out.println("pickupAddressCompanyFromJson = " + pickupAddressCompanyFromJson);
+        int pickupAddressCompanyFromLabelStart = decodedStringShipments.indexOf("^FT10,49^A0N,22,22^FD") + 21;
+        String pickupAddressCompanyFromLabel = decodedStringShipments.substring(pickupAddressCompanyFromLabelStart, pickupAddressCompanyFromLabelStart + pickupAddressCompanyFromJson.length());
+        //    System.out.println("pickupAddressContactFromLabel = " + pickupAddressCompanyFromLabel);
+        assertThat(pickupAddressCompanyFromJson.toUpperCase(), is(equalTo(pickupAddressCompanyFromLabel)));
+
+    }
+
+
+    @DisplayName("ZPL: Pickup Address Line 1 exists: Pickup Address Line 1")
+    @Test
+    public void test11() {
+
+        String pickupAddressLine1FromJson = fedExFusePojoShipments.getPickupAddress().getAddressLines().get(0).getValue();
+        //    System.out.println("pickupAddressLine1FromJson = " + pickupAddressLine1FromJson);
+        int pickupAddressLine1FromLabelStart = decodedStringShipments.indexOf("^FT10,93^A0N,22,22^FD") + 21;
+        String pickupAddressLine1FromLabel = decodedStringShipments.substring(pickupAddressLine1FromLabelStart, pickupAddressLine1FromLabelStart + pickupAddressLine1FromJson.length());
+        //    System.out.println("pickupAddressLine1FromLabel = " + pickupAddressLine1FromLabel);
+        assertThat(pickupAddressLine1FromJson, is(equalTo(pickupAddressLine1FromLabel)));
+
+    }
+
+
+    @DisplayName("ZPL: Pickup Address Line 2 exists: Pickup Address Line 2")
+    @Test
+    public void test12() {
+
+        String pickupAddressLine2FromJson = fedExFusePojoShipments.getPickupAddress().getAddressLines().get(1).getValue();
+        //    System.out.println("pickupAddressLine2FromJson = " + pickupAddressLine2FromJson);
+        int pickupAddressLine2FromLabelStart = decodedStringShipments.indexOf("^FT10,114^A0N,22,22^FD") + 22;
+        String pickupAddressLine2FromLabel = decodedStringShipments.substring(pickupAddressLine2FromLabelStart, pickupAddressLine2FromLabelStart + pickupAddressLine2FromJson.length());
+        //    System.out.println("pickupAddressLine2FromLabel = " + pickupAddressLine2FromLabel);
+        assertThat(pickupAddressLine2FromJson, is(equalTo(pickupAddressLine2FromLabel)));
+
+    }
+
+
+    @DisplayName("ZPL: Pickup Address Line 3 exists: City, State, ZIP")
+    @Test
+    public void test13() {
+
+        String pickupAddressLine3FromJson = fedExFusePojoShipments.getPickupAddress().getLocalityName() + ", " + fedExFusePojoShipments.getPickupAddress().getSubdivisionCode() + " " + fedExFusePojoShipments.getPickupAddress().getPostalCode();
+        //    System.out.println("pickupAddressLine3FromJson = " + pickupAddressLine3FromJson);
+        int pickupAddressLine3FromLabelStart = decodedStringShipments.indexOf("^FT10,136^A0N,22,22^FD") + 22;
+        String pickupAddressLine3FromLabel = decodedStringShipments.substring(pickupAddressLine3FromLabelStart, pickupAddressLine3FromLabelStart + pickupAddressLine3FromJson.length());
+        //    System.out.println("pickupAddressLine3FromLabel = " + pickupAddressLine3FromLabel);
+        assertThat(pickupAddressLine3FromJson.toUpperCase(), is(equalTo(pickupAddressLine3FromLabel)));
+
+    }
+
+
+    @DisplayName("ZPL: Pickup Address Country exists: Country Code")
+    @Test
+    public void test14() {
+
+        String pickupAddressLine4FromJson = fedExFusePojoShipments.getPickupAddress().getCountryCode();
+        //    System.out.println("pickupAddressLine4FromJson = " + pickupAddressLine4FromJson);
+        int pickupAddressLine4FromLabelStart = decodedStringShipments.indexOf("^FT10,158^A0N,22,22^FD") + 22;
+        String pickupAddressLine4FromLabel = decodedStringShipments.substring(pickupAddressLine4FromLabelStart, pickupAddressLine4FromLabelStart + pickupAddressLine4FromJson.length());
+        //    System.out.println("pickupAddressLine4FromLabel = " + pickupAddressLine4FromLabel);
+        assertThat(pickupAddressLine4FromJson, is(equalTo(pickupAddressLine4FromLabel)));
+    }
+
+
+    @DisplayName("ZPL: Ship Date exists: 2022-01-12T20:02:51.987Z")
+    @Test
+    public void test15() {
+
+        String shipDateFromJson = fedExFusePojoShipments.getRequestedPickupDateTime();
+        //    System.out.println("shipDateFromJson = " + shipDateFromJson);
+        String shipDayFromJson = shipDateFromJson.substring(8, 10);
+        String shipMonthFromJson = shipDateFromJson.substring(5, 7);
+        String shipYearFromJson = shipDateFromJson.substring(0, 4);
+        switch (shipMonthFromJson) {
+            case "01":
+                shipMonthFromJson = "JAN";
+                break;
+            case "02":
+                shipMonthFromJson = "FEB";
+                break;
+            case "03":
+                shipMonthFromJson = "MAR";
+                break;
+            case "04":
+                shipMonthFromJson = "APR";
+                break;
+            case "05":
+                shipMonthFromJson = "MAY";
+                break;
+            case "06":
+                shipMonthFromJson = "JUN";
+                break;
+            case "07":
+                shipMonthFromJson = "JUL";
+                break;
+            case "08":
+                shipMonthFromJson = "AUG";
+                break;
+            case "09":
+                shipMonthFromJson = "SEP";
+                break;
+            case "10":
+                shipMonthFromJson = "OCT";
+                break;
+            case "11":
+                shipMonthFromJson = "NOV";
+                break;
+            case "12":
+                shipMonthFromJson = "DEC";
+                break;
+        }
+        //    System.out.println("shipDayFromJson = " + shipDayFromJson);
+        //    System.out.println("shipMonthFromJson = " + shipMonthFromJson);
+        //    System.out.println("shipYearFromJson = " + shipYearFromJson);
+
+        int shipDateFromLabelStart = decodedStringShipments.indexOf("FT406,27^A0N,22,22^FDSHIP DATE: ") + 32;
+        String shipDateFromLabel = decodedStringShipments.substring(shipDateFromLabelStart, shipDateFromLabelStart + 7);
+        //    System.out.println("shipDateFromLabel = " + shipDateFromLabel);
+        assertThat(shipDayFromJson + shipMonthFromJson + shipYearFromJson.substring(2), is(equalTo(shipDateFromLabel)));
+    }
+
+
+    @DisplayName("Meter Number = 1711")
+    @Test
+    public void test16() {
+
+        assertThat(decodedStringShipments, containsString("^FT406,71^A0N,22,22^FDCAD: 1711/FUSE0008^FS"));
+    }
+
+
+    @DisplayName("Package Weight: GrossWeight + GrossWeightUnitOfMeasure")
+    @Test
+    public void test17() {
+
+        String packageWeightFromJson = String.format("%.2f", fedExFusePojoShipments.getShippingUnits().get(0).getGrossWeight());
+    //    System.out.println("packageWeightFromJson = " + packageWeightFromJson);
+        int packageWeightFromLabelStart = decodedStringShipments.indexOf("^FT406,49^A0N,22,22^FDACTWGT: ") + 30;
+        String packageWeightFromLabel = decodedStringShipments.substring(packageWeightFromLabelStart, packageWeightFromLabelStart + (packageWeightFromJson+ fedExFusePojoShipments.getShippingUnits().get(0).getGrossWeightUnitOfMeasure()).length());
+    //    System.out.println("packageWeightFromLabel = " + packageWeightFromLabel);
+        assertThat(packageWeightFromLabel, is(equalTo(packageWeightFromJson+ " " + fedExFusePojoShipments.getShippingUnits().get(0).getGrossWeightUnitOfMeasure().toUpperCase().trim())));
+    }
+
+
 }
+
+
+
+
+
